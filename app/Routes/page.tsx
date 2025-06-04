790aedca142b8d05443d695ffb26fe1e215591fb
hammadhafeez547 <mrhammadhafeez547@gmail.com>
2025-06-03 21:56:10 -0700

    module error fix

---
+++ app/Routes/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RoutesDetails from './components/RoutesDetails'
+
+function page() {
+  return (
+    <div>
+      <RoutesDetails />
+    </div>
+  )
+}
+
+export default page
